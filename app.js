d2b5914d7d9e016a620162e4b350cf33c166301f
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-05-19 16:24:18 +0100

    win function implemented with message and score when user wins

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score"); // DOM var
 const computerScore_span = document.getElementById("computer-score"); // DOM var
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -14,23 +14,47 @@ function getComputerChoice () {
   return choices[randomNumber];
 }
 
+function convertToWord (letter) {
+  if (letter === "r") return "Rock";
+  if (letter === "p") return "Paper";
+  return "Scissors";
+
+}
+
+function win (userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}. You win!`;
+}
+
+function lose () {
+  console.log("LOST");
+}
+
+function draw () {
+  console.log("DRAW");
+}
+
 function game (userChoice) {
   const computerChoice = getComputerChoice ();
   switch (userChoice + computerChoice) {
     case "rs":
     case "pr":
     case "sp":
-      console.log("USER WINS.");
+      win(userChoice, computerChoice);
       break;
     case "rp":
     case "ps":
     case "sr":
-      console.log("USER LOSES.");
+      lose();
       break;
     case "rr":
     case "pp":
     case "ss":
-      console.log("IT'S A DRAW.");
+      draw();
       break;
   }
 } 
